f6a2f60731677b6730b22aeda8bc5b7f5ba1971c
Charulatha-gopalakrishnan <113661621+Charulatha-gopalakrishnan@users.noreply.github.com>
2025-01-10 16:51:21 +0530

    Update App.java

--- gitLearn/src/main/java/com/gitLearn/gitLearn/App.java
+++ gitLearn/src/main/java/com/gitLearn/gitLearn/App.java
@@ -5,6 +5,6 @@ package com.gitLearn.gitLearn;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World hihi nandhu!");
     }
 }
